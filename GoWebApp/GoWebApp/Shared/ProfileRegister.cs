0a61a27ec87d69dee5d86b572afa64b0908d399b
Rok <57867569+rokdbest@users.noreply.github.com>
2021-03-15 18:23:10 +0100

    2 commit
    login register form

---
+++ GoWebApp/GoWebApp/Shared/ProfileRegister.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoWebApp.Shared
+{
+    public class ProfileRegister
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(16, ErrorMessage = "Username too long")]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Range(typeof(bool), "true", "true", ErrorMessage = "You have to confirm your profile")]
+        public bool isConfirmed { get; set; } = true;
+    }
+}
